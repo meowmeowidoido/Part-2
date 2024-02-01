7db289a89ad9024583efd4766523e8293d37e6d9
meowmeowidoido <haidaabb@shernet.sheridancollege.ca>
2024-02-01 10:28:10 -0500

    Task2 Commit: plane can move and follow the trail and rotate, and the trail disappears

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
@@ -10,11 +11,47 @@ public class Plane : MonoBehaviour
     public float newPositionThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D rigidbody;
+    public float speed = 1f;
     private void Start()
     {
        lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0,transform.position);
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+       currentPosition = transform.position;
+        if(points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPositionThreshold)
+            {
+                points.RemoveAt(0);
+                for(int i =0; i < lineRenderer.positionCount-2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i+1));
+
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
     private void  OnMouseDown()
     {
