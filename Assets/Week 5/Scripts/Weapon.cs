b7023ebfe8e913ab7b5f4fe8de013a9be298c1f3
Abbas Haidari <haidaabb@shernet.sheridancollege.ca>
2024-02-08 12:37:51 -0500

    Week5 Task4a complete: Axe destroys whenever it hits the player and deals damage and is destroyed if it does not have contact within 5 seconds

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    float timerSpawn;
+   
+    Vector2 moving = new Vector2(5, 0);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        collider.SendMessage("takeDamage", 1, SendMessageOptions.DontRequireReceiver);
+        Destroy(gameObject);
+    }
+   
+    
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position +moving * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        timerSpawn += Time.deltaTime;
+        if (timerSpawn > 5)
+        {
+            Destroy(gameObject);
+            timerSpawn = 0;
+        }
+    }
+}
